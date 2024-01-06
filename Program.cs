9cc410a7a6f815aee56835827ade1be57883e723
Alex <alex.dennett@students.plymouth.ac.uk>
2024-01-06 18:44:15 +0000

    v1.2, more minor bugfixes and attempting to correct for failed to host on server

--- Program.cs
+++ Program.cs
@@ -1,33 +1,27 @@
 using COMP_2001_Report.Data;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
     
-
 builder.Services.AddDbContext<UserDbContext>(options => 
                       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+//app.UseHttpsRedirection();
 
 app.MapControllers();
 
+app.UseAuthorization();
+
 app.Run();
+
